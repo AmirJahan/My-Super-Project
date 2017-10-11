a786b8b27d89dfdd9e920ce9f0f8db4a0f0269e3
Amir J <amir.jahan@gmail.com>
2017-10-11 12:03:54 -0700

    Initial Commit

---
+++ Sample Git/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sample Git
+//
+//  Created by Amir on 10/11/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
